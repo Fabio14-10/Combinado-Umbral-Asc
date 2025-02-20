65aa84755cff4ad2b5ff44891cf6d6c3f47347cf
Fabio14-10 <fabitoroa1110@gmail.com>
2025-02-19 20:22:53 -0500

    Update Asc.java

--- Combinado/src/combinado/Asc.java
+++ Combinado/src/combinado/Asc.java
@@ -4,17 +4,17 @@ package combinado;
 
 public class Asc {
 
-    public void moverAscensor(int piso) {
+    public void mover(int piso) {
             System.out.println("El ascensor se mueve al piso " + piso);
     }
     public double umbralAlerta=30.0;
     
-    public boolean verificar(double Temperatura){
+    public boolean Verificar(double Temperatura){
         return Temperatura > umbralAlerta;
     }
     
-    public void Aprobar(double Temperatura){
-        if (verificar(Temperatura)){
+    public void Acertar(double Temperatura){
+        if (Verificar(Temperatura)){
             System.out.println("¡ALERTA! La temperatura está por encima del umbral.");
         }
         else {
